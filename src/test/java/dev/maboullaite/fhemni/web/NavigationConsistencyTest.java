5af833f486c886175141c751703d75e16f9165d0
Mohammed Aboullaite <aboullaite.mohammed@gmail.com>
2026-09-14 13:30:55 +0200

    Center catalogue play icons (#44)

--- src/test/java/dev/maboullaite/fhemni/web/NavigationConsistencyTest.java
+++ src/test/java/dev/maboullaite/fhemni/web/NavigationConsistencyTest.java
@@ -133,7 +133,7 @@ class NavigationConsistencyTest {
         for (String page : ALL_PAGES) {
             assertThat(html(page))
                     .as("stylesheet in %s", page)
-                    .containsOnlyOnce("/css/dist.css?v=20260913-4");
+                    .containsOnlyOnce("/css/dist.css?v=20260914-1");
         }
     }
 
@@ -141,6 +141,15 @@ class NavigationConsistencyTest {
     void compactPartyBadgesOpticallyAlignTheirColourDot() throws IOException {
         assertThat(html("css/app.css"))
                 .contains(".party-badge .party-dot { transform: translateY(-1px); }");
+    }
+
+    @Test
+    void catalogPlayButtonUsesAFontIndependentCenteredTriangle() throws IOException {
+        assertThat(html("css/app.css"))
+                .contains(".catalog-thumbnail::after { content: \"\";")
+                .contains(".catalog-thumbnail::before { content: \"\";")
+                .contains("border-left: 10px solid var(--orange)")
+                .doesNotContain(".catalog-thumbnail::after { content: \"▶\"");
     }
 
     @Test
@@ -253,7 +262,7 @@ class NavigationConsistencyTest {
         for (String page : ADMIN_PAGES) {
             assertThat(html(page))
                     .as("mobile assets in %s", page)
-                    .contains("/css/dist.css?v=20260913-4")
+                    .contains("/css/dist.css?v=20260914-1")
                     .contains("/js/i18n.js?v=20260913-6");
         }
     }
@@ -310,7 +319,7 @@ class NavigationConsistencyTest {
                 .contains("id=\"programmeMedia\" class=\"programme-media\"")
                 .contains("class=\"video-js vjs-big-play-centered\"")
                 .contains("/webjars/video.js/8.23.8/dist/video-js.min.css")
-                .contains("/css/dist.css?v=20260913-4")
+                .contains("/css/dist.css?v=20260914-1")
                 .contains("/js/videojs-config.js?v=20260911-1")
                 .contains("/webjars/video.js/8.23.8/dist/video.min.js")
                 .contains("/js/i18n.js?v=20260913-6")
